c3b499544c76bbe8498440f4d83578d64e3a830c
Tony <tony3827@gmail.com>
2016-02-24 17:33:34 -0500

    added FlexEl

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var app = express();
 app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Personal website listening on port 3000!');
 });
