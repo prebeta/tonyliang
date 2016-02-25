d80d61e1ab84fd9acebfdf41c74aac1037b4329c
Tony <tony3827@gmail.com>
2016-02-25 09:18:56 -0500

    favico

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
